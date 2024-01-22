2cc79603498f46e9693979f8a24a1604c3a71eb8
Samba Carlson <sambacarlson@gmail.com>
2024-01-22 12:28:25 +0100

    Added basic interfaces for logic and persistce

--- src/models/models.go
+++ src/models/models.go
@@ -1,10 +1,14 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
 
 type Todo struct {
-	ID         int       `json:"id"`
-	UserID     int       `json:"user_id"`
+	ID         uuid.UUID `json:"id"`
+	UserID     uuid.UUID `json:"user_id"`
 	Heading    string    `json:"Heading"`
 	Body       string    `json:"body"`
 	CategoryID string    `json:"category"`
@@ -13,7 +17,7 @@ type Todo struct {
 }
 
 type Category struct {
-	ID          int       `json:"id"`
+	ID          uuid.UUID `json:"id"`
 	Title       string    `json:"title"`
 	Description string    `json:"description"`
 	CreatedAt   time.Time `json:"created_at"`
@@ -21,16 +25,18 @@ type Category struct {
 }
 
 type Reminder struct {
-	ID          int       `json:"id"`
-	TodoID      int       `json:"todo_id"`
-	RemindTime  time.Time `json:"remind_time"`
-	Description string    `json:"description"`
-	CreatedAt   time.Time `json:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at"`
+	ID            uuid.UUID     `json:"id"`
+	TodoID        uuid.UUID     `json:"todo_id"`
+	Snooze        bool          `json:"snooze"`
+	SnoozeTimeout time.Duration `json:"snooze_timeout"`
+	RemindTime    time.Time     `json:"remind_time"`
+	Description   string        `json:"description"`
+	CreatedAt     time.Time     `json:"created_at"`
+	UpdatedAt     time.Time     `json:"updated_at"`
 }
 
 type User struct {
-	ID        int       `json:"id"`
+	ID        uuid.UUID `json:"id"`
 	Username  string    `json:"username"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
--- src/presentation/presentation.go
+++ src/presentation/presentation.go
@@ -9,13 +9,86 @@ import (
 
 func Route(BASE_URL string) error {
 	router := gin.Default()
-	router.GET("/", func(c *gin.Context) {
-		c.String(200, time.Now().String())
+
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.String(200, time.Now().String())
 	})
 
-	router.GET("/status", func(c *gin.Context) {
-		c.JSON(http.StatusOK, "status: ok")
+	router.GET("/status", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, "status: ok")
 	})
 
+	// user enpoints
+	router.POST("/users", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, "")
+	})
+	router.GET("/users", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, "")
+	})
+	router.PATCH("/users/:id", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, "")
+	})
+	router.DELETE("/users/:id", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, "")
+	})
+	router.GET("/users/:id", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, "")
+	})
+
+	// todo enpoints
+	router.POST("/todos", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, "")
+	})
+	router.GET("/todos", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, "")
+	})
+	router.PATCH("/todos/:id", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, "")
+	})
+	router.DELETE("/todos/:id", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, "")
+	})
+	router.GET("/todos/:id", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, "")
+	})
+	router.GET("/todos/users/:id", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, "")
+	})
+
+	// reminder endpoints
+	router.POST("/reminders", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, "")
+	})
+	router.GET("/reminders", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, "")
+	})
+	router.PATCH("/reminders/:id", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, "")
+	})
+	router.DELETE("/reminders/:id", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, "")
+	})
+	router.GET("/reminders/:id", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, "")
+	})
+
+	// category endpoints
+	router.POST("/categories", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, "")
+	})
+	router.GET("/categories", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, "")
+	})
+	router.PATCH("/categories/:id", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, "")
+	})
+	router.DELETE("/categories/:id", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, "")
+	})
+	router.GET("/categories/:id", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, "")
+	})
+
+	// return
 	return router.Run(BASE_URL)
 }
